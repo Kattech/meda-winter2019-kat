b6cf6f4f4f46065927f19fc964f98ce89e6a9960
Kat Martin <smartinideas@example.com>
2019-10-21 11:43:42 -0700

    Backend class: Mon 21 Oct 2019. First day of class with javascript review: How to name files, variables, Datatypes: stings, Numbers, Boolean,Comparatiors, Flasey and Truthy. Also reviewed Programming Structures: -variables, -functions, -classes, -loops, & -if statements.

---
+++ practice/javascript/review.js
@@ -0,0 +1,99 @@
+//Storing things in Javascript 
+
+var nameOfVar = 1000;
+
+//Javascript DATATYPES:
+
+// Strings  *** ALWAYS *** wrapped in single or double matching quotes
+"Hello there!"
+
+// Number
+2910.234
+12399
+-32003; -1 * 32003;
+// (-)1 will cause an error b/c javascript recognizes it as a
+
+// Boolean 
+true;
+false;
+// Boolean Operator:
+// ! Opposite **OJO: to be confused with a comparitor
+
+//*****DO NOT: 
+// Do not use key words to name things ie: true and false are reserved words for booleans and can not be used as variable names
+// Do not start w/ #
+// var 10Hello - 1000;
+// Recommended no start with caps.
+var Capital = "hello";
+//No spaces in naming
+//var first word = "hello";
+
+//Camel Case
+var helloThereHowAreYou = 100;
+
+// Snake Case
+var hello_there_how_are_you = 40;
+
+// Math
+// + adds, concatination
+// - subraction
+// * multiplication
+// / division
+// % modulo
+
+//Comparitors
+// == Is the same as
+// ===
+// > Greater than
+// < Lesser than
+// >= Greater or equal
+//<= Lesser or equal
+// != not equal
+
+// Boolean Operator
+// ! Opposite
+
+// Falsey Values:
+false;
+undefined; //goes to a false code block
+null; // nothing is being held
+Nan; //
+0;
+"" //empty string  ***OJO: " " <--NOT an empty string b/c it holds a space and space is a character**
+
+
+// Truthy
+// Everything else
+
+
+//PROGRAMMING STRUCTURES
+// variables
+// functions
+// classes
+// loops
+// If statements
+
+objects
+functions
+classes
+
+// If Statement
+if (/* condition */ false) {
+    /* code to run if it is true. */
+} else {
+    /* code to run if it is false. */
+}
+else if 
+
+}
+
+
+//Loops:
+//For Loops:
+for ( initialization; Condition; iteration ) {}
+
+//Function:
+function nameOfFunction(/* parameters */){
+    /* code block */
+}
+//**OJO: Functions can be written anywhere in the code. All can go in the beginning or the end. It can also be sprinkled in between but this is not recommended
