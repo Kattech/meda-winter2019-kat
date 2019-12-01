f0beba2bf04fba451af73d5f8668eced377e6891
Kat Martin <smartinideas@example.com>
2019-11-30 23:00:06 -0800

    Dataset project using NY dataset of MVA (Motor Vehicle Accidents

---
+++ homework/dataset/nyMVA2014.js
@@ -0,0 +1,12 @@
+// The following dataset was obtained Data.gov and reflect hourly in military time
+https://catalog.data.gov/dataset/motor-vehicle-crashes-case-information-beginning-2009
+
+//QUESTIONS:
+// 1. What time of day do accidents most frequently occur?
+// 2. Do lighting conditions impact accidents- if so, how?
+// 3. Which county had the most and least accidents?
+// 4. How do road surface conditions affect the topography aka Road Description
+// 5. When there are accidents involving 3 cars or more, is it because of road conditions?
+// 6. What is the number of accidents that have the following: injury, property damage or both?
+// 7. Do accidents with deer occur more in the morning or the evening?
+    
